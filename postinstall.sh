7036ce112e9c419e7973e10b700106db59eb9a64
Ben-Hur Santana <besali96@gmail.com>
2025-04-20 23:01:42 -0300

    tentando deploy

--- postinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Installing Playwright browsers..."
-playwright install
-echo "Playwright install completed."
-
-# Se estiver em um ambiente Linux (como o Streamlit Cloud)
-echo "Installing system dependencies..."
-playwright install-deps
-echo "Dependencies installed."
